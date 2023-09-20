fc32fa190cd7eaf4efe4b348794caaef442e28bf
Bhairav <bhairavgotam066@gmail.com>
2023-09-20 16:44:27 +0530

    added frontend and backend

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+import AllRoutes from './components/AllRoutes';
+
+function App() {
+  return (
+    <div>
+       <Navbar/>
+        <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
